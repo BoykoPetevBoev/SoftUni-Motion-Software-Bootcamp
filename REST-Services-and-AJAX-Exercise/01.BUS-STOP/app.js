13fb6910bea745ed8179fca15f206dd0925636a3
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-12-19 00:49:31 +0200

    update

--- REST-Services-and-AJAX-Exercise/01.BUS-STOP/app.js
+++ REST-Services-and-AJAX-Exercise/01.BUS-STOP/app.js
@@ -1,3 +1,41 @@
-function getInfo() {
-    console.log("TODO...");
-}
+async function getInfo() {
+    const stopId = getElement('stopId').value;
+    const data = await requester(stopId);
+
+    getElement('buses').innerHTML = '';
+
+    if(data.error){
+        getElement('stopName').innerHTML = 'Error';
+        return;
+    }
+    getElement('stopName').innerHTML = data.name;
+    getElement('buses').append(...createList(data.buses));
+    getElement('stopId').value = '';
+
+    function createList(buses) {
+        return Object
+            .keys(buses)
+            .map(bus => createElement('li', `Bus ${bus} arrives in ${buses[`${bus}`]} minutes`))
+    }
+
+    function createElement(type, value) {
+        const element = document.createElement(type);
+
+        if (value) element.innerHTML = value;
+        return element;
+    }
+
+    function getElement(id) {
+        const element = document.getElementById(id);
+        
+        if (!element) throw new Error('Missing DOM element! #' + id);
+        return element;
+    }
+
+    async function requester(id) {
+        const response = await fetch(`https://judgetests.firebaseio.com/businfo/${id}.json`);
+        const data = await response.json();
+        console.table(data);
+        return data;
+    }
+}   
--- REST-Services-and-AJAX-Exercise/02.BUS-SCHEDULE/app.js
+++ REST-Services-and-AJAX-Exercise/02.BUS-SCHEDULE/app.js
@@ -1,17 +1,41 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+    let name = '';
+    let next = 'depot';
 
+    async function depart() {
+        buttonsManager(true);
+        const data = await requester(next);
+        getElement('span-info').innerHTML = `Next stop ${data.name}`;
+        name = data.name;
+        next = data.next;
+    }
     function arrive() {
-        console.log('Arrive TODO...');
+        buttonsManager(false);
+        getElement('span-info').innerHTML = `Arriving at ${name}`;
     }
-
     return {
         depart,
         arrive
     };
+}
+
+function buttonsManager(change) {
+    getElement('depart').disabled = change;
+    getElement('arrive').disabled = !change;
+}
+
+function getElement(id) {
+    const element = document.getElementById(id);
+    if (!element) throw new Error('Missing DOM element! #' + id);
+    return element;
+}
+
+async function requester(id) {
+    const response = await fetch(`https://judgetests.firebaseio.com/schedule/${id}.json`);
+    const data = await response.json();
+    console.table(data);
+    return data;
 }
 
 let result = solve();
