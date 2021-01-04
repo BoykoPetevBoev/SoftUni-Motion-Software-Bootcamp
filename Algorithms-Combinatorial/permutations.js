126313ac771080a11d084bb32566c663bf448f57
BoykoPetevBoev <boykopetevboev@gmail.com>
2021-01-04 18:43:55 +0200

    Permutations

---
+++ Algorithms-Combinatorial/permutations.js
@@ -0,0 +1,26 @@
+function printResult(result) {
+    console.log(result.join(' '));
+}
+
+function recursion(elements, result, index) {
+
+    if (index === elements.length) printResult(result);
+
+    for (let i = 0; i < elements.length; i++) {
+
+        if (!result.includes(elements[i])) {
+
+            result[index] = elements[i];
+            recursion(elements, result, index + 1);
+            result.pop();
+        }
+    }
+}
+
+function permutations(elements) {
+
+    recursion(elements, [], 0);
+}
+
+
+permutations(['A', 'B', 'C']);
